b31a4cd1fb51dd982b21aba37b4147821c59ffd5
ASherkaL <112230938+menghan-liu-18@users.noreply.github.com>
2022-09-26 15:58:09 -0400

    change js

--- hw4-solution/js/detailPrice.js
+++ hw4-solution/js/detailPrice.js
@@ -91,6 +91,50 @@ function updatePrice() {
 
 let cartArr = [];
 
+
+// Update gallery links
+const queryString = window.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const chosenRoll = params.get('roll');
+
+const baseNumber = document.querySelector(".conclu-text");
+
+const currentRoll = {
+    currentTag: chosenRoll,
+    currName: "Original cinnamon roll",
+    currBasePrice: 2.49,
+    currFile: "original-cinnamon-roll.jpg",
+}
+
+for (var key in rolls) {
+    if (chosenRoll == key) {
+        currentRoll.currBasePrice = rolls[key].basePrice;
+        currentRoll.currFile = rolls[key].imageFile;
+    } else if (chosenRoll == "Double chocolate") {
+        currentRoll.currBasePrice = 3.99;
+        currentRoll.currFile = "double-chocolate-cinnamon-roll.jpg";
+    }
+};
+
+// update name
+const subTitle = document.querySelector('#product-name');
+if(currentRoll.currentTag == null) {
+    subTitle.innerText = "Original cinnamon roll";
+} else {
+    subTitle.innerText = currentRoll.currentTag + " cinnamon roll";
+}
+
+// update image
+const proImage = document.querySelector('.detail-img');
+proImage.src = "products/" + currentRoll.currFile;
+
+// write the current 
+const base = document.querySelector(".conclu-text");
+base.innerText = '$ ' + currentRoll.currBasePrice.toString();
+
+
 // Store info
 class Roll {
     constructor(rollType, rollGlazing, packSize, basePrice) {
