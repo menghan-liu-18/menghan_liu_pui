7b65af2c0206179a020386a33deead1e6763ae02
ASherkaL <112230938+menghan-liu-18@users.noreply.github.com>
2022-10-04 15:26:23 -0400

    first draft

---
+++ hw5-solution/js/detailPrice.js
@@ -0,0 +1,166 @@
+// create glazing items
+class ItemG {
+    itemGlazingName;
+    itemGlazingValue;
+
+    constructor(itemGlazingName, itemGlazingValue) {
+        this.itemGlazingName = itemGlazingName;
+        this.itemGlazingValue = itemGlazingValue;
+    }
+};
+
+// create pack size items
+class ItemP {
+    itemPackSize;
+    itemPackValue;
+
+    constructor(packSize, packValue) {
+        this.itemPackSize = packSize;
+        this.itemPackValue = packValue;
+    }
+}
+
+let keepOriginal = new ItemG ("Keep original", 0);
+let sugarMilk = new ItemG ("Sugar milk", 0);
+let vanillaMilk = new ItemG ("Vanilla milk", 0.5);
+let doubleCho = new ItemG ("Double chocolate", 1.5);
+
+let glazingArr = [keepOriginal, sugarMilk, vanillaMilk, doubleCho];
+
+let selectGlazing = document.querySelector('#glazing-select');
+
+// display glazing options
+for (i = 0;i<4;i++) {
+    var option = document.createElement('option');
+    option.text = glazingArr[i].itemGlazingName;
+    option.value = glazingArr[i].itemGlazingValue;
+    selectGlazing.add(option);
+};
+
+let size1 = new ItemP ("1", 1);
+let size3 = new ItemP ("3", 3);
+let size6 = new ItemP ("6", 5);
+let size12 = new ItemP ("12", 10);
+
+let sizeArr = [size1, size3, size6, size12];
+
+let selectSize = document.querySelector('#pack-size');
+
+// display pack size options
+for (i = 0;i<4;i++) {
+    var option = document.createElement('option');
+    option.text = sizeArr[i].itemPackSize;
+    option.value = sizeArr[i].itemPackValue;
+    selectSize.add(option);
+};
+
+// the current one
+const curr = {
+    currGText: "Keep Original",
+    currGlaze: 0,
+    currPText: "1",
+    currPack: 1,
+};
+
+function glazingChange (element) {
+    const priceChange = element.value;
+    curr.currGlaze = Number(priceChange);
+    const text = element[element.selectedIndex].text;
+    curr.currGText = text;
+    
+    updatePrice();
+};
+
+function packChange (element) {
+    const text = element[element.selectedIndex].text;
+    const packPrice = element.value;
+    curr.currPack = Number(packPrice);
+    curr.currPText = text;
+
+    updatePrice();
+}
+
+// Update the price
+function updatePrice() {
+    let price = (currentRoll.currBasePrice + curr.currGlaze) * curr.currPack;
+
+    const concluText = document.querySelector('.conclu-text');
+
+    concluText.innerText = '$ ' + price.toFixed(2);
+};
+
+let cartArr = [];
+
+
+// Update gallery links
+const queryString = window.location.search;
+
+const params = new URLSearchParams(queryString);
+
+let chosenRoll = params.get('roll');
+
+if(chosenRoll == "Double-chocolate") {
+    chosenRoll = "Double chocolate";
+};
+
+const baseNumber = document.querySelector(".conclu-text");
+
+const currentRoll = {
+    currentTag: chosenRoll,
+    currName: "Original cinnamon roll",
+    currBasePrice: 2.49,
+    currFile: "original-cinnamon-roll.jpg",
+}
+
+for (var key in rolls) {
+    if (chosenRoll == key) {
+        currentRoll.currBasePrice = rolls[key].basePrice;
+        currentRoll.currFile = rolls[key].imageFile;
+    } else if (chosenRoll == "Double chocolate") {
+        currentRoll.currBasePrice = 3.99;
+        currentRoll.currFile = "double-chocolate-cinnamon-roll.jpg";
+    }
+};
+
+// update name
+const subTitle = document.querySelector('#product-name');
+if(currentRoll.currentTag == null) {
+    subTitle.innerText = "Original cinnamon roll";
+} else {
+    subTitle.innerText = currentRoll.currentTag + " cinnamon roll";
+}
+
+// update image
+const proImage = document.querySelector('.detail-img');
+proImage.src = "products/" + currentRoll.currFile;
+
+// write the current 
+const base = document.querySelector(".conclu-text");
+base.innerText = '$ ' + currentRoll.currBasePrice.toString();
+
+
+// Store info
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+};
+
+function addToCart() {
+    const glazText = document.querySelector('#glazing-select');
+    
+    const packText = document.querySelector('#pack-size');
+    
+    let detailRoll = new Roll(currentRoll.currentTag, curr.currGText, curr.currPText, currentRoll.currBasePrice);
+
+    cartArr.push(detailRoll);
+
+    for(i = 0; i < cartArr.length; i++) {
+        console.log(cartArr[i]);
+    };
+};
+
+document.querySelector('.add').addEventListener('click', addToCart);
---
+++ hw5-solution/js/shop.js
@@ -0,0 +1,130 @@
+const cart = new Set();
+
+class Roll {
+    totelP;
+
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;
+    }
+}
+
+const default1 = new Roll("Original", "Sugar milk", 1, 2.49);
+const default2 = new Roll("Walnut", "Vanilla milk", 12, 3.49);
+const default3 = new Roll("Raisin", "Sugar milk", 3, 2.99);
+const default4 = new Roll("Apple", "Original", 3, 3.49);
+
+cart.add(default1);
+cart.add(default2);
+cart.add(default3);
+cart.add(default4);
+
+// create glazing items
+class ItemG {
+    itemGlazingName;
+    itemGlazingValue;
+
+    constructor(itemGlazingName, itemGlazingValue) {
+        this.itemGlazingName = itemGlazingName;
+        this.itemGlazingValue = itemGlazingValue;
+    }
+};
+
+// create pack size items
+class ItemP {
+    itemPackSize;
+    itemPackValue;
+
+    constructor(packSize, packValue) {
+        this.itemPackSize = packSize;
+        this.itemPackValue = packValue;
+    }
+}
+
+// set glazing and price
+
+let original = new ItemG ("Original", 0);
+let sugarMilk = new ItemG ("Sugar milk", 0);
+let vanillaMilk = new ItemG ("Vanilla milk", 0.5);
+let doubleCho = new ItemG ("Double chocolate", 1.5);
+let glazingArr = [original, sugarMilk, vanillaMilk, doubleCho];
+
+let size1 = new ItemP ("1", 1);
+let size3 = new ItemP ("3", 3);
+let size6 = new ItemP ("6", 5);
+let size12 = new ItemP ("12", 10);
+let sizeArr = [size1, size3, size6, size12];
+
+// calculate price for one item
+function updateOnePrice(roll) {
+    let currG = "Original";
+    let currP = 1;
+
+    for (i = 0;i<4;i++) {
+        if(roll.glazing == glazingArr[i].itemGlazingName) {
+            currG = glazingArr[i].itemGlazingValue;
+        };
+    };
+    for(i = 0;i<4;i++) {
+        if(roll.size == sizeArr[i].itemPackSize) {
+            currP = sizeArr[i].itemPackValue;
+        };
+    };
+
+    let price = (roll.basePrice + currG) * currP;
+    return(price.toFixed(2));
+    // console.log(price.toFixed(2));
+} 
+
+// create total price
+let totalPrice = 0;
+
+// add new roll
+function addNewRoll(rollType, rollGlazing, packSize, rollPrice) {
+    const newRoll = new Roll(rollType, rollGlazing, packSize, rollPrice);
+    cart.add(newRoll);
+};
+
+for (const roll of cart) {
+    createRoll(roll);
+};
+
+function createRoll (roll) {
+    const template = document.querySelector('#cart-sum-template');
+    const clone = template.content.cloneNode(true);
+    roll.element = clone.querySelector('.cart-summ');
+
+    const cartRightList = document.querySelector('.cart-right');
+    cartRightList.prepend(roll.element);
+
+    updateRollList(roll);
+};
+
+function updateRollList(roll) {
+    const cartImg = document.querySelector('.cart-img');
+    const setRight = document.querySelector('.set-right');
+    let summText = document.createElement('p');
+    summText.className = "summ-text";
+    let summP = document.createElement('p');
+    summP.className = "summ-price";
+    setRight.prepend(summP);
+    setRight.prepend(summText);
+
+    const imgURL = "products/"+roll.type.toLowerCase()+"-cinnamon-roll.jpg";
+    cartImg.src = imgURL;
+    summText.innerText = roll.type + " Cinnamon Roll" + '\n' + "Glazing: " + roll.glazing + '\n' + "Pack Size: " + roll.size;
+    let priceOne = updateOnePrice(roll);
+    summP.innerText = "$ " + priceOne;
+    totalPrice += parseFloat(priceOne);
+
+    updatePriceAll();
+};
+
+console.log(totalPrice);
+
+function updatePriceAll() {
+    const totalText = document.querySelector('.total-price');
+    totalText.innerText = "$ " +  totalPrice;
+}
