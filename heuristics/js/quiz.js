61e76132c12e31ac17f99af97f66ed7e49983218
ASherkaL <112230938+menghan-liu-18@users.noreply.github.com>
2022-11-21 17:27:48 -0500

    transition

--- heuristics/js/quiz.js
+++ heuristics/js/quiz.js
@@ -6,7 +6,7 @@ console.log(chosenQ);
 function answerClick(que) {
     const choiceL = document.getElementById('nav-btn-left');
     const choiceR = document.getElementById('nav-btn-right');
-    const intro = document.getElementById('intro');
+    const intro = document.getElementById('intro-text');
     const nextQ = document.getElementById("next");
     
     if(que.Cor == "A1") {
@@ -79,7 +79,7 @@ function answerClick(que) {
 function submitClick(que) {
     const sbm = document.getElementById('submit');
     const ans = document.getElementById('ans');
-    const intro = document.getElementById('intro');
+    const intro = document.getElementById('intro-text');
     const nextQ = document.getElementById("next");
     
     sbm.onclick = function() {
@@ -102,13 +102,35 @@ function submitClick(que) {
     }
 }
 
+function changeColor(num) {
+    j = num - 2;
+    for(i = 0;i <= j;i++) {
+        const proNextC = document.querySelectorAll('.circle')[i];
+        proNextC.style.backgroundColor = "#379956";
+        if((j > 0) && (i < j)){
+            const proNextL = document.querySelector('.line');
+            proNextL.classList.remove("line");
+            proNextL.classList.add("line-static");
+        }
+    }
+
+    const proNextL = document.querySelector('.line');
+    proNextL.classList.add("line2");    
+
+    const currC = document.querySelectorAll('.circle')[num-1];
+    var delayInMilliseconds = 2000;
+    setTimeout(function() {
+        currC.style.backgroundColor = "#FFC85B";
+    }, delayInMilliseconds);
+}
+
 function updateQ() {
+    const heading = document.querySelector('h1');
+    const intro = document.getElementById('intro-text');
 
     if(chosenQ == '1') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 1';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No1.Prompt;
 
         const leftBtn = document.getElementById('nav-btn-left');
@@ -121,29 +143,26 @@ function updateQ() {
     }
 
     if(chosenQ == '2') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 2';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No2.Prompt;
 
         const leftBtn = document.getElementById('nav-btn-left');
         leftBtn.innerText = question.No2.A1;
         leftBtn.style.fontSize = "22px";
-        // leftBtn.style.lineHeight = "3px"
 
         const rightBtn = document.getElementById('nav-btn-right');
         rightBtn.innerText = question.No2.A2;
         rightBtn.style.fontSize = "22px";
 
+        changeColor(2);
+
         answerClick(question.No2);
     }
 
     if(chosenQ == '3') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 3';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No3.Prompt;
 
         const leftBtn = document.getElementById('nav-btn-left');
@@ -152,14 +171,14 @@ function updateQ() {
         const rightBtn = document.getElementById('nav-btn-right');
         rightBtn.innerText = question.No3.A2;
 
+        changeColor(3);
+
         answerClick(question.No3);
     }
 
     if(chosenQ == '4') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 4';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No4.Prompt;
 
         const leftBtn = document.getElementById('nav-btn-left');
@@ -168,24 +187,24 @@ function updateQ() {
         const rightBtn = document.getElementById('nav-btn-right');
         rightBtn.innerText = question.No4.A2;
 
+        changeColor(4);
+
         answerClick(question.No4);
     }
 
     if(chosenQ == '5') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 5';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No5.Prompt;
         
+        changeColor(5);
+
         submitClick(question.No5);
     }
 
     if(chosenQ == '6') {
-        const heading = document.querySelector('h1');
         heading.innerText = 'Question 6';
 
-        const intro = document.getElementById('intro');
         intro.innerText = question.No6.Prompt;
 
         const leftBtn = document.getElementById('nav-btn-left');
@@ -197,6 +216,8 @@ function updateQ() {
         rightBtn.innerText = question.No6.A2;
         rightBtn.style.fontSize = "22px";
         rightBtn.style.lineHeight = "20px";
+
+        changeColor(6);
 
         answerClick(question.No6);
     }
