4c4d689d2a84e687c05ec34ab4f5027fcb34b399
ASherkaL <112230938+menghan-liu-18@users.noreply.github.com>
2022-11-29 20:09:27 -0500

    animation

---
+++ heuristics/js/home-ane.js
@@ -0,0 +1,11 @@
+$ (function () {
+	$('h1').textillate({ 
+        in: { effect: 'bounceIn'}});
+});
+
+$ (function () {
+	$('.intro-home').textillate({ 
+        initialDelay: 1500,
+        in: { effect: 'fadeInUp', sync: true}});
+});
+
--- heuristics/js/result.js
+++ heuristics/js/result.js
@@ -1,5 +1,7 @@
 let ans = initializeNum();
 
+// let ansArr = initializeArr();
+
 function initializeNum() {
     let ansNum = 0;
 
@@ -13,6 +15,19 @@ function initializeNum() {
     return ansNum;
 }
 
+// function initializeArr() {
+//     let ansArr = [];
+
+//     if (localStorage.getItem('ansNum') != null) {
+//         const getAnsNum = localStorage.getItem('ansNum');
+//         ansNum = JSON.parse(getAnsNum);
+//     } else {
+//         ansNum = 0;
+//     } 
+
+//     return ansNum;
+// }
+
 function updateNumHeu() {
     const numHue = document.getElementById("num-heu");
     numHue.innerText = ans;
@@ -23,5 +38,28 @@ function updateNumLo() {
     numLog.innerText = 6 - ans;
 }
 
-updateNumHeu();
-updateNumLo();
+const graph1 = document.getElementById("graph1");
+
+function updateGraph() {
+    const graph1 = document.getElementById("graph1");
+    const graph2 = document.getElementById("graph2");
+    const getAnsNum = localStorage.getItem('ansNum');
+    ans = JSON.parse(getAnsNum);
+    const pA = (ans/6)*100;
+    const pAS = pA.toString() + '%';
+    const pL = ((6-ans)/6)*100;
+    const pLS = pL.toString() + '%';
+    console.log(pAS);
+    console.log(pLS);
+
+    // graph1.style.background = "conic-gradient(#E8CEAE ${pA}%, #E8E7E7 ${pL}%)";
+    graph2.style.background = "conic-gradient(#E8CEAE 60%, #E8E7E7 40%)";
+}
+
+function update(){
+    updateNumHeu();
+    updateNumLo();
+    updateGraph()
+}
+
+update();
